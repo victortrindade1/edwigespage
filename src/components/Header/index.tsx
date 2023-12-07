acfaa2edafdd0c9b57cfc7b9b54177f67c678150
Victor Trindade <victortrindade@gmail.com>
2023-12-07 12:32:18 -0300

    Photo with svg mask and border

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
-
+import Image from "next/image";
+import logo from "../../assets/svg/logo.svg";
 import styles from "./styles.module.css";
 
 const Header: React.FC = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <Image src={logo} alt='Logomarca' width={100} height={24} />
+    </div>
+  );
 };
 
 export default Header;
