0d4f78721d8837a0abad8f86c3df8214dd092c44
Victor Trindade <victortrindade@gmail.com>
2023-12-19 16:09:14 -0300

    Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,33 @@
+import Image from "next/image";
 import React from "react";
 
+import logo from "@/assets/svg/logowhite.svg";
 import styles from "./styles.module.css";
 
 const Footer: React.FC = () => {
-  return <footer className={styles.footerContainer}></footer>;
+  return (
+    <footer className={styles.footerContainer}>
+      <div className={styles.footerBody}>
+        <div className={styles.logoContainer}>
+          <Image src={logo} alt={"Logo"} className={styles.logo} />
+        </div>
+        <div className={styles.descriptionContainer}>
+          <h3>Dr. Ed - Médico Veterinário</h3>
+          <p>
+            Dr. Edwiges Siqueira
+            <br />
+            CRMV-RJ 9809
+            <br />
+          </p>
+          <h3>Contato</h3>
+          <p>Telefone / Whatsapp: (22) 99782-7997</p>
+          <br />
+          <h3>Campos dos Goytacazes e Região</h3>
+        </div>
+      </div>
+      <div className={styles.website}>www.dred.vercel.app</div>
+    </footer>
+  );
 };
 
 export default Footer;
