0314ca7a42958feab1fa6c31806d48793085a9ab
Victor Trindade <victortrindade@gmail.com>
2023-12-19 22:10:34 -0300

    Vercel Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
-import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import type { Metadata } from "next";
 import { rubik } from "./fonts";
 // import "animate.css/animate.min.css";
 import "./globals.css";
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
